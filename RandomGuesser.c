18b6b831276447a9d4380bc47ac47650d9acb617
Michael Short <michaelshort@Michaels-MacBook-Air.local>
2021-06-07 13:12:22 +0930

    cleaned up code

--- RandomGuesser.c
+++ RandomGuesser.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
 int main(int argc, char **argv)
 {
-	int max = atoi(argv[1]);
-	srand(time(0));
-	int randomVar = rand() % max + 1;
-	int guess;
+	puts("test");
+
+	int maximum_number = atoi(argv[1]);
+	int target_number = rand() % maximum_number + 1;
+	bool guess_is_wrong;
 
 	do 
 	{
-		printf("Guess a number between 1 and: %d: ", max);
+		int guess;
+		printf("Guess a number between 1 and: %d: ", maximum_number);
 		scanf("%d", &guess);
 
-		if(guess < randomVar) {
-			puts("Too low");
-		} else if(guess > randomVar) {
-			puts("Too High");
-		}
+		bool too_low = guess < target_number;
+		bool too_high = guess > target_number;
+		guess_is_wrong = too_low || too_high;
+
+		if(too_low) puts("Too Low");
+		if(too_high) puts("Too High");
+
 		
-	} while(guess != randomVar);
+	} while(guess_is_wrong);
 
 	puts("Hurray You Win!");
 
